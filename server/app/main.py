07637e63e6c20b512dc532d9ee5bc6a5e376e933
utak3r <piotr.borys@gmail.com>
2026-03-10 13:50:26 +0100

    CORS support added

--- server/app/main.py
+++ server/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 import os
 from app.api import recipes, tags
 
 
 app = FastAPI(title="Baza przepisów kulinarnych")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 os.makedirs("uploads", exist_ok=True)
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
